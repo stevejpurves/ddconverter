60e14b140f7458becf615ddb75b0b4243f8796bf
stevejpurves <stevejpurves@gmail.com>
2014-05-19 09:26:12 +0100

    first steps with angularjs app from scratch

---
+++ public/app.js
@@ -0,0 +1,9 @@
+var ddapp  = angular.module("ddapp",[]);
+
+//ddapp.controller("Converter", ['$scope', function($scope){
+//        $scope.outputName = "Decimal Degrees From Model";
+//}]);
+
+ddapp.controller("Converter", function(){
+    this.outputName = "Decimal Degrees From Model";
+});
