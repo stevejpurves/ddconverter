ec10acba7fea9d14c347f40aed7f8397c2bd0cd7
Steve Purves <stevejpurves@gmail.com>
2014-06-20 15:59:36 +0100

    RED mulitple lines

--- public/coordinates.spec.js
+++ public/coordinates.spec.js
@@ -50,9 +50,9 @@ function convert(input) {
         var matched_sets = extractDegreesMinutesSeconds(input);
         var output_string = "";
         for (var i = 0; i < matched_sets.length; i++) {
-            if (i > 0) output_string += "\n";
             var tokens = parseDegreesMinutesSeconds(matched_sets[i]);
             var latlong = LatLong( dms2dd(tokens.slice(2)), dms2dd(tokens.slice(7)) );
+            if (i > 0) output_string += "\n";
             output_string += latlong.toString();
         }
         return output_string;
@@ -90,6 +90,7 @@ describe("Converting coordinates to Decimal Degrees",function(){
 
         describe("multiple entries",function(){
             itIsAnExpectedConversion("1 1 1.1 2 2 2.2\n3 3 3.3 4 4 4.4","1.01972, 2.03944\n3.05917, 4.07889");
+            itIsAnExpectedConversion("1 1 1.1 2 2 2.2 3 3 3.3 4 4 4.4","1.01972, 2.03944 3.05917, 4.07889");
         });
 
         describe("Handling each of Degrees, Minutes and Decimal Seconds",function(){
